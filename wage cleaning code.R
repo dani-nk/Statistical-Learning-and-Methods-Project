a060437b6fa998040a35c42eaa977ed451bf7ebb
dani-nk <dani.neuharthkeusch@gmail.com>
2018-04-18 10:49:54 -0500

    removed some weird error looking code and symbols i didn't put in there

--- wage cleaning code.R
+++ wage cleaning code.R
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 library(base)
 data<-X1035PM_4_3_18_2008_2018_PERM
 data$country_of_origin <- toupper(data$country_of_origin)
@@ -15,7 +14,6 @@ table(data$pw_unit_of_pay)
 
 #Translate all units of pay into yearly using the following translations: YR = 2000 HR, 50 WK, 25 BI, 12 MTH
 data$annual_wage <- ifelse(data$pw_unit_of_pay=="HR",data$pw_amount*2000,data$pw_amount)
-=======
 data<-X1035PM_4_3_18_2008_2018_PERM
 toupper(data$country_of_origin)
 toupper(data$pw_unit_of_pay)
@@ -45,13 +43,11 @@ data$pw_unit_of_pay <- replace(data$pw_unit_of_pay,data$pw_unit_of_pay=="YEAR","
 data$pw_unit_of_pay <- replace(data$pw_unit_of_pay,data$pw_unit_of_pay=="MONTH","MTH")
 
 data$annual_wage <- ifelse(data$pw_unit_of_pay=="HR",data$pw_amount*2000,data$annual_wage)
->>>>>>> 4a752186da2f20786e1ecb3d649df14e47162dec
 data$annual_wage <- ifelse(data$pw_unit_of_pay=="WK",data$pw_amount*50,data$annual_wage)
 data$annual_wage <- ifelse(data$pw_unit_of_pay=="BI",data$pw_amount*25,data$annual_wage)
 data$annual_wage <- ifelse(data$pw_unit_of_pay=="MTH",data$pw_amount*12,data$annual_wage)
 View(data)
 
-<<<<<<< HEAD
 #exclude values below 9,600 annual wage, which were entered incorrectly in original data as the wrong unit and are not actually annual wage
 newdata_no_typo_wages<-subset(data,annual_wage>=9600)
 #check
@@ -60,9 +56,7 @@ View(newdata_no_typo_wages)
 X1035PM_4_3_18_2008_2018_PERM<-newdata_no_typo_wages
 View(X1035PM_4_3_18_2008_2018_PERM)
 
-=======
 newdata_no_typo_wages<-subset(data,annual_wage>=9600)
 View(newdata_no_typo_wages)
 X1035PM_4_3_18_2008_2018_PERM<-newdata_no_typo_wages
 View(X1035PM_4_3_18_2008_2018_PERM)
->>>>>>> 4a752186da2f20786e1ecb3d649df14e47162dec
