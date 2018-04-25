b24568f76952832bc19a1da3f33364b71bc4c8fd
dani-nk <dani.neuharthkeusch@gmail.com>
2018-04-25 15:56:10 -0500

    LDA model code

--- analyses/LDA.R
+++ analyses/LDA.R
@@ -7,19 +7,20 @@ train <- sample(1:nrow(visasclean), 0.7*nrow(visasclean))
 visasclean.train <- visasclean[train ,]
 visasclean.test <- visasclean[-train ,] 
 
-#part b#
+#LDA
 library(MASS)
 attach(visasclean)
 lda<-lda(case_status_new~annual_wage,visasclean.train)
-lda2<-lda(case_status_new~annual_wage+as.factor(country_new)+as.factor(naics_code_new)+as.factor(job_state_new), visasclean.train)
-lda3<-lda(case_status_new~annual_wage+as.factor(naics_code_new)+as.factor(class_new)+as.factor(job_state_new)+gdp_per_cap+pop_muslim+migration_per_cap+unemployment)
+lda2<-lda(case_status_new~annual_wage+as.factor(country_new)+as.factor(naics_code_new)+as.factor(job_state_new), visasclean.train, na.action = na.omit)
 lda.prediction<-predict(lda2,visasclean.test)
+detach(visasclean)
 
-warnings()
+#prediction
+names(lda.prediction)
+lda.class=lda.prediction$class
+table(lda.class,visasclean.test$case_status_new)
+mean(lda.class==visasclean.test$case_status_new)
+mean(lda.class!=visasclean.test$case_status_new)
 
-#names(lda.prediction)
-#lda.class=lda.prediction$class
-#table(lda.class,case_status_new)
-#(206+207)/1599
-#mean(lda.class==case_status_new)
-#mean(lda.class!=case_status_new)
+#add in more variables
+lda3<-lda(case_status_new~annual_wage+as.factor(naics_code_new)+as.factor(class_new)+as.factor(job_state_new)+gdp_per_cap+pop_muslim+migration_per_cap+unemployment)
