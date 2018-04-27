dc4ba13848dcbd0170fd6fa6b1c5360f97e815ce
dani-nk <dani.neuharthkeusch@gmail.com>
2018-04-27 16:53:15 -0500

    updated lda and qda script

--- analyses/LDAandQDA.R
+++ analyses/LDAandQDA.R
@@ -3,12 +3,13 @@
 set.seed(1)
 train <- sample(1:nrow(visasclean), 0.7*nrow(visasclean))
 visasclean.train <- visasclean[train ,]
-visasclean.test <- visasclean[-train ,] 
+visasclean.test <- visasclean[-train ,]
 
-#LDA
+#LDA (using country_new instead of other collinear country predictors)
 library(MASS)
 attach(visasclean)
 lda<-lda(case_status_new~annual_wage+decision_year+as.factor(country_new)+as.factor(naics_code_new)+as.factor(job_state_new), visasclean.train)
+lda
 
 #prediction
 lda.prediction<-predict(lda,visasclean.test)
@@ -17,34 +18,24 @@ names(lda.prediction)
 lda.class=lda.prediction$class
 table(lda.class,visasclean.test$case_status_new)
 mean(lda.class==visasclean.test$case_status_new)
-mean(lda.class!=visasclean.test$case_status_new)
+mean(lda.class!=visasclean.test$case_status_new) # 8.35% test error rate
 
-#add in more variables, removing country_new because of collinearity with other country data
-attach(visasclean)
-lda2<-lda(case_status_new~annual_wage+decision_year+as.factor(naics_code_new)+as.factor(class_new)+as.factor(job_state_new)+gdp_per_cap+pop_muslim+migration_per_cap+unemployment,visasclean.train)
-lda.prediction2<-predict(lda2,visasclean.test)
-detach(visasclean)
-names(lda.prediction2)
-lda.class2=lda.prediction2$class
-table(lda.class2,visasclean.test$case_status_new)
-mean(lda.class2==visasclean.test$case_status_new)
-mean(lda.class2!=visasclean.test$case_status_new)
 #Type 1 error (false positive -- certified as denied)
-1751/154357 #1.1%
+1895/154357 #1.22%
 #Type 2 error (false negative -- denied as certified)
-10819/154357 #7%
+10999/154357 #7.13%
 
 #QDA
 attach(visasclean)
-qda<-qda(case_status_new~annual_wage+decision_year+as.factor(naics_code_new)+as.factor(class_new)+as.factor(job_state_new)+gdp_per_cap+pop_muslim+migration_per_cap+unemployment,visasclean.train)
+qda<-qda(case_status_new~annual_wage+decision_year+as.factor(naics_code_new)+as.factor(class_new)+as.factor(job_state_new)+as.factor(country_new),visasclean.train)
 detach(visasclean)
 qda
 qda.prediction<-predict(qda,visasclean.test)
 qda.class=qda.prediction$class
 table(qda.class,visasclean.test$case_status_new)
 mean(qda.class==visasclean.test$case_status_new)
-mean(qda.class!=visasclean.test$case_status_new)
+mean(qda.class!=visasclean.test$case_status_new) #20.43% test error
 #Type 1 error (false positive -- certified as denied)
-22887/154357 #14.8%
+24648/154357 #15.87%
 #Type 2 error (false negative -- denied as certified)
-7178/154357 #4.65%
+6884/154357 #4.46%
